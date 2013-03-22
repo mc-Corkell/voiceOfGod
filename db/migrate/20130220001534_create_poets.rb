8de97f173f68d6a97df92b85b3caa67f86bfba6d
mc-Corkell <katiemccorkell@gmail.com>
2013-03-22 16:53:15 -0700

    readme

--- db/migrate/20130220001534_create_poets.rb
+++
@@ -1,9 +0,0 @@
-class CreatePoets < ActiveRecord::Migration
-  def change
-    create_table :poets do |t|
-      t.string :poetName #must be unique, use first and lastname as one word, eg: KatieMcCorkell
-      t.integer :phoneNumber
-      t.timestamps
-    end
-  end
-end
